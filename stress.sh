c99c0c531dc03b90f7fec33d9311613a1b9fcebd
hej81107 <hayden_juday@yahoo.com>
2025-03-16 22:12:45 +0000

    Added sleep to stress & added it to systemd

--- stress.sh
+++ stress.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 while true; do
      echo "Starting stress test..."
-    
-    # CPU Stress (2 workers for 30s)
+
+    #cpu
     stress-ng --cpu 2 --timeout 30s &
 
-    # IO Stress (2 workers for 30s)
+    #i/o
     stress-ng --io 2 --timeout 30s &
 
-    # Memory Stress (2 workers, 256MB each for 30s)
-    stress-ng --vm 2 --vm-bytes 400M --timeout 30s &
+    #memory
+    stress-ng --vm 2 --vm-bytes 350M --timeout 30s &
 
-    # Filesystem Stress (2 workers for 30s)
+    #disk_usage
     stress-ng --hdd 2 --timeout 30s &
 
-    # Network Stress (2 workers for 30s)
+    #network
     stress-ng --sock 2 --timeout 30s &
 
     wait
     echo "Stress test complete." 
+    sleep 30
 done
